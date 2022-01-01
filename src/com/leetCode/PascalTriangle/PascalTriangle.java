fd66d20ca530b986dec34f92e19d03fb5155b92b
shannonsullivan <shannons4252@gmail.com>
2021-12-31 19:58:45 -0500

    Commit

---
+++ src/com/leetCode/PascalTriangle/PascalTriangle.java
@@ -0,0 +1,24 @@
+package com.leetCode.PascalTriangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+    public static List<List<Integer>> generate(int numRows){
+
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+
+            for (int j = row.size() -1; j >= 1; j--) {
+
+                row.set(j, row.get(j) + row.get(j-1));
+
+            }
+            row.add(1);
+            result.add(new ArrayList<>(row));
+        }
+        return result;
+    }
+}
