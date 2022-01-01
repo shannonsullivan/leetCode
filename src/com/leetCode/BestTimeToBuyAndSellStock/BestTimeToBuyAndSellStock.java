64ee3ac6a46cf3c4076b4b90268f2d27e3711416
shannonsullivan <shannons4252@gmail.com>
2021-12-31 20:30:32 -0500

    Commit

---
+++ src/com/leetCode/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,20 @@
+package com.leetCode.BestTimeToBuyAndSellStock;
+
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+
+        int minValue = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+
+            if (prices[i] < minValue) {
+                minValue = prices[i];
+
+            } else if (prices[i] - minValue > maxProfit) {
+                maxProfit = prices[i] - minValue;
+            }
+        }
+        return maxProfit;
+    }
+}
