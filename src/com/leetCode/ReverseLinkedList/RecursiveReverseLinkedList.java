fafe3236ab483cfbc00e48e839c2eaea414d5844
shannonsullivan <shannons4252@gmail.com>
2022-01-02 14:40:24 -0500

    Commit

---
+++ src/com/leetCode/ReverseLinkedList/RecursiveReverseLinkedList.java
@@ -0,0 +1,18 @@
+package com.leetCode.ReverseLinkedList;
+
+public class RecursiveReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        return reverse(head, null);
+    }
+
+    private ListNode reverse(ListNode head, ListNode newHead) {
+        if (head == null) return newHead;
+
+        ListNode tempNext = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = tempNext;
+
+        return reverse(head, newHead);
+    }
+}
