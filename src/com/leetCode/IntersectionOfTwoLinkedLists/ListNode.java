d599fb1b828025e3ad3041c4f86ba046a52abe55
shannonsullivan <shannons4252@gmail.com>
2022-01-01 23:22:44 -0500

    Commit

---
+++ src/com/leetCode/IntersectionOfTwoLinkedLists/ListNode.java
@@ -0,0 +1,12 @@
+package com.leetCode.IntersectionOfTwoLinkedLists;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
