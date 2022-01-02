09583a5d74e0cfbc8a994399353ab15ce5a89b31
shannonsullivan <shannons4252@gmail.com>
2022-01-02 18:28:18 -0500

    Commit

---
+++ src/com/leetCode/DeleteNodeInLinkedList/DeleteNodeInLinkedList.java
@@ -0,0 +1,9 @@
+package com.leetCode.DeleteNodeInLinkedList;
+
+public class DeleteNodeInLinkedList {
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
---
+++ src/com/leetCode/DeleteNodeInLinkedList/ListNode.java
@@ -0,0 +1,11 @@
+package com.leetCode.DeleteNodeInLinkedList;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
