93aeac5c39f9429d924ab3b376389c939f20d8f5
shannonsullivan <shannons4252@gmail.com>
2022-01-03 00:38:57 -0500

    Commit

---
+++ src/com/leetCode/PalindromeLinkedList/PalindromeLinkedList.java
@@ -0,0 +1,39 @@
+package com.leetCode.PalindromeLinkedList;
+
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode reverseHead = reverse(slow);
+
+        while (reverseHead != null && head != null) {
+            if (reverseHead.val != head.val) {
+                return false;
+            } else {
+                reverseHead = reverseHead.next;
+                head = head.next;
+            }
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+
+        ListNode previous = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = previous;
+            previous = head;
+            head = next;
+        }
+        return previous;
+    }
+}
