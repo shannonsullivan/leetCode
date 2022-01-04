fa93a5be6c69443a6d242c23256cb86afad83a12
shannonsullivan <shannons4252@gmail.com>
2022-01-04 13:14:50 -0500

    Commit

---
+++ src/com/leetCode/ConvertSortedArrayToBinarySearchTree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,21 @@
+package com.leetCode.ConvertSortedArrayToBinarySearchTree;
+
+public class ConvertSortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length -1);
+    }
+
+    private TreeNode dfs(int[] nums, int start, int end) {
+        if (end < start) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, start, mid -1);
+        root.right = dfs(nums, mid +1, end);
+
+        return root;
+    }
+}
