ed23e16db03157fd870989b086560f9bca78818e
shannonsullivan <shannons4252@gmail.com>
2022-02-02 20:38:47 -0500

    Arrays 101 - Update code without comments and add README with pseudocode

---
+++ src/com/leetCode/Arrays/RemoveElement/RemoveElement.java
@@ -0,0 +1,15 @@
+package com.leetCode.Arrays.RemoveElement;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int length = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[length] = nums[i];
+                length++;
+            }
+        }
+        return length;
+    }
+}
