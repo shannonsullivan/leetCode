b05d6cedc101700e95768a9b58a6231809d1ecec
shannonsullivan <shannons4252@gmail.com>
2022-05-02 08:17:05 -0400

    ValidAnagram added HashMap

--- src/com/leetCode/arrays/validAnagram/ValidAnagram.java
+++ src/com/leetCode/arrays/validAnagram/ValidAnagram.java
@@ -1,8 +1,10 @@
 package com.leetCode.arrays.validAnagram;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ValidAnagram {
+
     public boolean isAnagram(String s, String t) {
 
         if (s.length() != t.length()) return false;
@@ -14,5 +16,26 @@ public class ValidAnagram {
         Arrays.sort(tChar);
 
         return Arrays.equals(sChar, tChar);
+    }
+
+    public boolean isAnagramHash(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+        if (s.equals(t)) return true;
+
+        HashMap<Character, Integer> stringSMap = new HashMap<>();
+        HashMap<Character, Integer> stringTMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            stringSMap.merge(s.charAt(i), 1, Integer::sum);
+            stringTMap.merge(t.charAt(i), 1, Integer::sum);
+        }
+
+        for (Character c : stringSMap.keySet()) {
+            if (!stringSMap.get(c).equals(stringTMap.get(c))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
